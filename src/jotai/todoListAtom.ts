70b9f45eccc1db4c983939048f6dc98e95d0805f
Cattleya <Cattleya.dev.0502@gmail.com>
2023-08-13 23:25:12 +0900

    [mod]各Todo単位で弄れるように修正、入力値をstateに保存できるよう修正

--- src/jotai/todoListAtom.ts
+++ src/jotai/todoListAtom.ts
@@ -1,17 +1,22 @@
 import { atom } from "jotai";
+import { splitAtom } from "jotai/utils";
 
-export type TodoListItem = {
+export type TodoListItemsAtom = {
   title: string;
   due: string;
   isFineshed: boolean;
-  index: number;
-};
+}[];
 
-const testItem:TodoListItem = {
-  title:'test',
+const testItems:TodoListItemsAtom = [{
+  title:'test1',
+  due: '2023-01-01',
+  isFineshed:false,
+},{
+  title:'test2',
   due: '',
   isFineshed:false,
-  index:0
-}
+}]
 
-export const todoListAtom = atom<TodoListItem[]>([testItem]);
+
+const todoItemsAtom = atom(testItems);
+export const todoItemAtomsAtom = splitAtom(todoItemsAtom);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import TodoList from './components/pages/todoList.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TodoList from "./components/pages/todoList.tsx";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Provider } from "jotai";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TodoList />
-  </React.StrictMode>,
-)
+    <Provider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <TodoList />
+      </LocalizationProvider>
+    </Provider>
+  </React.StrictMode>
+);
