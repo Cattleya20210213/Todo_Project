a6465836e6abe796ebb1f9165b6905f39d0fa056
Cattleya <Cattleya.dev.0502@gmail.com>
2023-08-20 15:11:56 +0900

    [mod]不要な定義を削除

--- src/jotai/atoms.ts
+++ src/jotai/atoms.ts
@@ -1,7 +1,6 @@
 import { atom } from "jotai";
 import { atomWithStorage, splitAtom } from "jotai/utils";
 import { TodoItemInfo } from "./type/todoItemInfo";
-import { SearchCriteliaTodo } from "./type/searchCriteriaTodo";
 
 export const initTodoListItem:TodoItemInfo = {
   title:'',
@@ -17,13 +16,5 @@ export const isDeleteTodoItemAtom = atom(false);
 export const todoListAtom = atomWithStorage<TodoItemInfo[]>('todo_list',[]);
 export const todoItemAtomsAtom = splitAtom(todoListAtom);
 export const todoItemToAddAtom = atom(initTodoListItem);
-
-export const initSearchCriteliaTodo:SearchCriteliaTodo = {
-  isFinish:false,
-  title:'',
-  matchPatternTitle:'',
-}
-
-export const searchCriteliaAtom = atomWithStorage<SearchCriteliaTodo>('search_critelia', initSearchCriteliaTodo);
 
 export type AtomSetter<T> = (arg_0:T) => void;
